4866ab5f5cb1f9676ab2aa452a741133292ccda2
David Mongeau-Petitpas <dmongeau_petitpas@ecoutez.ca>
2012-11-23 16:34:26 -0500

    Working server

--- server.js
+++ server.js
@@ -14,29 +14,17 @@ var app = express();
 var twitter = require('ntwitter');
 var twit = new twitter(CONFIG.twitter);
 
-//Bitly
-var Bitly = require('bitly');
-var bitly = new Bitly(CONFIG.bitly.username, CONFIG.bitly.key);
-
 //Create mongodb link
-var mongoose = require('mongoose');
-mongoose.connect(CONFIG.mongoURL);
+//var mongoose = require('mongoose');
+//mongoose.connect(CONFIG.mongoURL);
 
-//When running localy use the non builded folder
-if(!process.env.NODE_ENV || process.env.NODE_ENV != 'production') {
-	var publicFolder = __dirname + "/app";
-
-//When running nodejitsu use builded folder
-} else {
-	var publicFolder = __dirname + "/dist";
-}
+var publicFolder = __dirname + "/dist";
 
 //Template engine
 var consolidate = require('consolidate');
 app.engine('html', consolidate.swig);
 app.set('view engine', 'html');
 app.set('views', publicFolder);
-
 
 //Configure express
 app.configure(function() {
