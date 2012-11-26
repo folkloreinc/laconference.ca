5b67babd0686648d417c04f338fd0fbfa4af1174
Francois Cote <francois@francoiscote.net>
2012-11-26 16:37:28 -0500

    remove kinetic, added frameAnimation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,40 @@
 require.config({
     paths: {
-        jquery: 'vendor/jquery.min',
-        kinetic: 'vendor/kinetic-v4.0.5.min'
-    },
-
-    shim: {
-        kinetic: {
-            exports: function() { return Kinetic; }
-        }
+        jquery: 'vendor/jquery.min'
     }
 
 });
  
-require(['jquery', 'kinetic', 'app'], function($, kinetic, app) {
-    // use app here
-    console.log($, kinetic, app);
+require(['jquery', 'frameAnimation'], function($, FrameAnimation) {
+    
+    // console.log($);
+
+    // var socket = io.connect('http://localhost');
+    //     socket.on('news', function (data) {
+    //     console.log(data);
+    //     socket.emit('my other event', { my: 'data' });
+    // });
+
+    var cercle1Animation = new FrameAnimation({
+        'frames': [
+            'images/frameAnimations/Cercle1-01.png',
+            'images/frameAnimations/Cercle1-02.png',
+            'images/frameAnimations/Cercle1-03.png',
+            'images/frameAnimations/Cercle1-04.png',
+            'images/frameAnimations/Cercle1-05.png',
+            'images/frameAnimations/Cercle1-06.png',
+            'images/frameAnimations/Cercle1-07.png',
+            'images/frameAnimations/Cercle1-08.png',
+            'images/frameAnimations/Cercle1-09.png',
+            'images/frameAnimations/Cercle1-10.png'
+        ],
+        'loop': false
+    });
+
+    cercle1Animation.animate();
+
+    
+
+
+
 });
