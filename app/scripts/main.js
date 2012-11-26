f31793e636a81a844d9031ace67782749d6367a3
Francois Cote <francois@francoiscote.net>
2012-11-25 20:41:33 -0500

    indentation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,18 @@
 require.config({
-  paths: {
-    jquery: 'vendor/jquery.min',
-    kinetic: 'vendor/kinetic-v4.0.5.min'
-  },
+    paths: {
+        jquery: 'vendor/jquery.min',
+        kinetic: 'vendor/kinetic-v4.0.5.min'
+    },
 
-  shim: {
-    kinetic: {
-        exports: function() { return Kinetic; }
+    shim: {
+        kinetic: {
+            exports: function() { return Kinetic; }
+        }
     }
-  }
 
 });
  
-require(['jquery', 'kinetic', 'app'], function(jquery, kinetic, app) {
-  // use app here
-  console.log(jquery, kinetic, app);
+require(['jquery', 'kinetic', 'app'], function($, kinetic, app) {
+    // use app here
+    console.log($, kinetic, app);
 });
